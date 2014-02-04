3d3eb01b6aad5462dc6b0d8789bc4c8cefef8be7
Alex Fish <alex@alexefish.com>
2014-02-04 21:51:44 +0000

    Implementing repo and exec types in init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,19 @@
+# Public: Install kiwi xcode templates
+#
+# Examples
+#
+#   include kiwi
 class kiwi {
-  anchor { 'Hello_World': }
+
+    $path = '/tmp/kiwi'
+    $install_file = "${path}/Xcode Templates/install-templates.sh"
+
+    repository { $path:
+        source   => 'allending/Kiwi',
+        provider => 'git';
+    }
+
+    exec { $install_file:
+        require => File[$install_file],
+    }
 }
